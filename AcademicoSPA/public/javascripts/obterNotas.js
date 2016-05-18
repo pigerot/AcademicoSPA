0f8c79bfc312cea7b6207a34d687db0fdb13468d
Pedro Freitas <pedrogdf@gmail.com>
2016-05-18 11:26:20 -0300

    Initial commit

---
+++ AcademicoSPA/public/javascripts/obterNotas.js
@@ -0,0 +1,11 @@
+var app = angular.module('academicoSPA', []);
+app.controller('ObterNotas', ObterNotas);
+
+function ObterNotas($scope, $http) {
+    $http.get('http://localhost:8080/AcademicoSPARestful/academico/obternotas').
+        success(function(data) {
+            $scope.alunoNotas = data;
+        }).error(function(data, status, headers, config) {
+			alert( "failure");
+		});
+}
